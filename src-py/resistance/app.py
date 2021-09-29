ec40bb84dcbcf2dea9da6f3d41f3414548c0ac3b
Jasper Paterson <jasperpaterson@Jaspers-MacBook-Pro.local>
2021-09-29 15:51:33 +0800

    flask untested

--- src-py/resistance/app.py
+++ src-py/resistance/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, redirect
-from first_agent import FirstAgent
+from flask import Flask, render_template, request, redirect, url_for
+from first_agent.first_agent import FirstAgent
 
 app = Flask("Resistance")
 agent = FirstAgent()
@@ -8,13 +8,24 @@ turn = 0
 @app.route("/", methods=["GET", "POST"])
 def main():
     if(request.method == "POST"):
+        if not request.form.get["players"] or not request.form.get["number"]:
+            return render_template("index.html")
         number_of_players = request.form["players"]
-        
-
+        player_number = request.form["number"]
+        spy_list = []
+        for i in range(10):
+            if request.form.get[str(i)]:
+                if request.form.get[str(i)] >= number_of_players:
+                    return render_template("index.html")
+                spy_list.append(i)
+        if len(spy_list) == 0:
+            return redirect("propose.html")
+        if len(spy_list) == agent.spy_count[number_of_players] and request.form.get[str(player_number)]:
+            return redirect("propose.html")
     return render_template("index.html")
 
 @app.route("/propose", methods=["GET", "POST"])
-def turn():
+def propose():
     return render_template("propose.html")
 
 if __name__ == "__main__":

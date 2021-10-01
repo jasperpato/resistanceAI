26b7f21b3c3dba25e58df503823db6b80f938008
Jasper Paterson <jasperpaterson@Jaspers-MacBook-Pro.local>
2021-10-01 10:02:10 +0800

    debugging results

--- src-py/resistance/results.py
+++ src-py/resistance/results.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randrange
 from game import *
 
 '''
@@ -7,23 +7,19 @@ Simulate games and print aggregates
 f_wins = 0
 r_wins = 0
 for i in range(10000):
-    n = randint(0,5)
-    g = Game([RandomAgent(str(j)) for j in range(n)] + [FirstAgent(str(5-j)) for j in range(5-n)])
+    n = randrange(6)
+    g = Game([RandomAgent(str(j)) for j in range(n)] + [FirstAgent(str(j)) for j in range(n, 5)])
     g.play()
     if g.missions_lost < 3:
-        for j in range(g.num_players):
+        for j in range(5): 
             if j not in g.spies:
-                if j < n:
-                    r_wins += 1
-                else:
-                    f_wins += 1
+                if isinstance(g.agents[j], FirstAgent): f_wins += 1
+                else: r_wins += 1
     else:
-        for j in range(g.num_players):
+        for j in range(5): 
             if j in g.spies:
-                if j < n:
-                    r_wins += 1
-                else:
-                    f_wins += 1
+                if isinstance(g.agents[j], FirstAgent): f_wins += 1
+                else: r_wins += 1
 
 print(f"FirstAgent won {f_wins} times.")
 print(f"RandomAgent won {r_wins} times.")

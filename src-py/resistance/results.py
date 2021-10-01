fa56670b3bb09aa2804531d20ffb7d7a587d0a45
Jasper Paterson <jasperpaterson@Jaspers-MacBook-Pro.local>
2021-10-01 10:04:50 +0800

    debugging results

--- src-py/resistance/results.py
+++ src-py/resistance/results.py
@@ -10,16 +10,13 @@ for i in range(10000):
     n = randrange(6)
     g = Game([RandomAgent(str(j)) for j in range(n)] + [FirstAgent(str(j)) for j in range(n, 5)])
     g.play()
-    if g.missions_lost < 3:
-        for j in range(5): 
-            if j not in g.spies:
-                if isinstance(g.agents[j], FirstAgent): f_wins += 1
-                else: r_wins += 1
-    else:
-        for j in range(5): 
-            if j in g.spies:
-                if isinstance(g.agents[j], FirstAgent): f_wins += 1
-                else: r_wins += 1
+    for j in range(5): 
+        if j not in g.spies and g.missions_lost < 3:
+            if isinstance(g.agents[j], FirstAgent): f_wins += 1
+            else: r_wins += 1
+        elif j in g.spies and g.missions_lost >= 3:
+            if isinstance(g.agents[j], FirstAgent): f_wins += 1
+            else: r_wins += 1
 
 print(f"FirstAgent won {f_wins} times.")
 print(f"RandomAgent won {r_wins} times.")
